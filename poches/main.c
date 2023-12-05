51b51778812ecae77ed47707728f6b413b4db8fb
Julien BONZOM <bonzomjulien40@gmail.com>
2023-12-05 17:34:33 +0100

    Firstmodif Branche approcheBranche

--- poches/main.c
+++ poches/main.c
@@ -3,36 +3,41 @@
 #define MIN(i, j) (((i) < (j)) ? (i) : (j))
 #define MAX(i, j) (((i) > (j)) ? (i) : (j))
 
-void extremaAbsciss(int table[],int dimTable,int* tableExtrema[]);
-int calculateBiggestVolumLake(int table[],int dimTable,int tableExtrema[],int dimTableExtrema);
+void extremaAbsciss(int table[],int dimTable,int tableExtrema[],int* dimTableExtremaptr);
+int calculateBiggestVolumLake(int table[],int dimTable,int tableExtrema[],int* dimTableExtremaptr);
 
 int main()
 {
-    int table[10]={0,1,3,0,5,2,1,8,2,0};
-    int dimTable=sizeof(table);
+    int table[]={0,1,3,0,5,2,1,8,2,0};
+    int dimTable=sizeof(table)/sizeof(table[0]);
 
-    int* tableExtrema[0];
+    int tableExtrema[50];
+    int dimTableExtrema=sizeof(tableExtrema)/sizeof(tableExtrema[0]);
+    int* dimTableExtremaptr = &dimTableExtrema;
+
+
+
     int volumeMax= 0;
 
-    extremaAbsciss(table,dimTable,tableExtrema);
-    printf("%d", sizeof(tableExtrema));
+    extremaAbsciss(table,dimTable,tableExtrema, dimTableExtrema);
 
-    int dimTableExtrema=sizeof(tableExtrema);
-    volumeMax=calculateBiggestVolumLake(table,dimTable,tableExtrema,dimTableExtrema);
+
+
+    volumeMax=calculateBiggestVolumLake(table,dimTable,tableExtrema,dimTableExtremaptr);
 
     printf("Le volume maximum est egal à %d.\n",volumeMax);
 
     return 0;
 }
 
-void extremaAbsciss(int table[],int dimTable,int* tableExtrema[])
+void extremaAbsciss(int table[],int dimTable,int tableExtrema[],int* dimTableExtremaptr)
 {
     int n =0;
     int firstLevel=0;
-    int lastLeevel=0;
+    int lastLevel=0;
     int increment =0;
     int j =0;
-    int variable =0;
+
 
     for(int i=0;i<dimTable;i++)
     {
@@ -40,7 +45,7 @@ void extremaAbsciss(int table[],int dimTable,int* tableExtrema[])
         if(table[i]>firstLevel)     //Determination du niveau des bornes laterales
         {
             firstLevel=table[i];
-            lastLeevel=table[i];
+            lastLevel=table[i];
 
 
         }
@@ -49,43 +54,47 @@ void extremaAbsciss(int table[],int dimTable,int* tableExtrema[])
     }
 
     table[0]=firstLevel;
-    table[dimTable-1]=lastLeevel;
+    table[dimTable-1]=lastLevel;
 
     //tableExtrema[0]=0;
 
 
-
+    //printf("%d",dimTable);
     for(j=1;j<dimTable-1;j++) //determination des minimas
 
     {
-        int boolCondition = ((table[j-1])>=(table[j])) && ((table[j])<=(table[j+1]));
+        //scanf("%d", &variable);
+        //printf("%d ", j);
+        int boolCondition = ((table[j-1])>(table[j])) && ((table[j])<(table[j+1]));
         if (boolCondition)
         {
-            increment++;
-            //printf("%d %d %d\n", boolCondition, table[j],  increment);
+
+            //printf("%d %d %d\ %d \n", boolCondition, table[j],  increment, j);
             //scanf("%d", &variable);
-            tableExtrema = (int *) realloc( tableExtrema, (increment) * sizeof(int) );
+
             tableExtrema[increment]= j;
+
             printf("%d \n", tableExtrema[increment]);
+            increment++;
 
         }
 
     }
 
-    tableExtrema = (int *) realloc( tableExtrema, (increment+1) * sizeof(int) );
-    tableExtrema[increment+1]=dimTable;
+    dimTableExtremaptr=increment;
+
 
 
 
 
 }
-int calculateBiggestVolumLake(int table[],int dimTable,int tableExtrema[],int dimTableExtrema)
+int calculateBiggestVolumLake(int table[],int dimTable,int tableExtrema[],int* dimTableExtremaptr)
 {
     int volumeMax = 0;
     int volume = 0;
     int const cst =1;
-
-    for(int i=0;i<dimTableExtrema;i++)
+    int variable =0;
+    for(int i=0;i<dimTableExtremaptr;i++)
     {
 
         int incrementRight =1;
@@ -118,6 +127,7 @@ int calculateBiggestVolumLake(int table[],int dimTable,int tableExtrema[],int di
             incrementLeft++;
 
             }
+
             boolCondition = (((table[tableExtrema[i]-incrementRight]) >= (table[tableExtrema[i]])) || ((table[tableExtrema[i]+incrementLeft]) >= (table[tableExtrema[i]])));
             boolCondition2 = (((table[tableExtrema[i]-incrementRight]) >= (table[tableExtrema[i]])) && ((table[tableExtrema[i]+incrementLeft]) >= (table[tableExtrema[i]])));
             boolCondition3 = (((table[tableExtrema[i]-incrementRight]) >= (table[tableExtrema[i]])) && !((table[tableExtrema[i]+incrementLeft]) >= (table[tableExtrema[i]])));
